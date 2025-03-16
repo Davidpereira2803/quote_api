b68011d8d464770c7dc27b6a3d4c9919c04700da
Davidpereira2803 <david.pereira.002@student.uni.lu>
2025-03-16 23:57:12 +0100

    feat: enhance quote loading functionality with language support and error handling

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Query,  HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import json
 import random
@@ -14,9 +14,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-def load_quotes():
-    with open("lux_data.json", "r", encoding="utf-8") as file:
-        return json.load(file)
+def load_quotes(language: str):
+    file_map = {
+        "lux": "lux_data.json",
+        "en": "en_data.json"
+    }
+
+    try:
+        with open(file_map[language], "r", encoding="utf-8") as file:
+            return json.load(file)
+    except (FileNotFoundError, KeyError):
+        raise HTTPException(status_code=400, detail="Invalid language parameter")
+    except json.JSONDecodeError:
+        raise HTTPException(status_code=500, detail="Error loading quotes data")
 
 class QuoteRequest(BaseModel):
     text: str
@@ -25,20 +35,20 @@ class QuoteRequest(BaseModel):
     meaning: str
 
 @app.get("/quotes")
-def get_all_quotes():
-    quotes = load_quotes()
+def get_all_quotes(lang: str = Query("lux", regex="^(lux|en)$")):
+    quotes = load_quotes(lang)
     return quotes
 
 @app.get("/quote")
-def get_random_quote():
-    quotes = load_quotes()
+def get_random_quote(lang: str = Query("lux", regex="^(lux|en)$")):
+    quotes = load_quotes(lang)
     if not quotes:
         raise HTTPException(status_code=404, detail="No quotes found")
     return random.choice(quotes)
 
 @app.get("/quotes/category/{category}")
-def get_quote_by_category(category: str):
-    quotes = [q for q in load_quotes() if q["category"].lower() == category.lower()]
+def get_quote_by_category(category: str, lang: str = Query("lux", regex="^(lux|en)$")):
+    quotes = [q for q in load_quotes(lang) if q["category"].lower() == category.lower()]
     if not quotes:
         raise HTTPException(status_code=404, detail="No quotes found in this category")
     return random.choice(quotes)
