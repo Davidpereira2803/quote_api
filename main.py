bcc03d690c95587ed25a1545854ecce74e8de27c
Davidpereira2803 <david.pereira.002@student.uni.lu>
2025-03-16 23:05:49 +0100

    feat: add entry point for running the application with uvicorn

--- main.py
+++ main.py
@@ -48,3 +48,8 @@ def add_quote(quote: QuoteRequest):
         json.dump(quotes, file, indent=4)
 
     return {"message": "Quote added successfully", "quote": new_quote}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
